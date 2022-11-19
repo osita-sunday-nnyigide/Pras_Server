8dd3cd7e1599db1335625ccfe6cab29010322df8
osita-sunday-nnyigide <37690739+osita-sunday-nnyigide@users.noreply.github.com>
2022-11-20 04:14:43 +0900

    Update shell.sh

--- Pras_Server_C++/shell.sh
+++ Pras_Server_C++/shell.sh
@@ -4,10 +4,9 @@ target="pdb/$1"
 
 for f in "$target"*
 do
-    ./PRAS -i $f -f xxxx -r yes -m yes -l no -h yes -p no -s yes
+    ./PRAS -i $f -f xxxx -r yes -m yes -l no -h no -p no -s no
     python3 PlotStructure.py jet 
     echo "fixed" $(basename $f)
-    printf $(basename $f)"\n">> rec.txt
     echo " "
 done
 
