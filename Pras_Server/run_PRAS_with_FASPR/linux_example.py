a4003828b776ec49cfe1881f5b54897c7427bcba
osita-sunday-nnyigide <37690739+osita-sunday-nnyigide@users.noreply.github.com>
2023-04-17 21:52:55 +0900

    Update linux_example.py

--- Pras_Server/run_PRAS_with_FASPR/linux_example.py
+++ Pras_Server/run_PRAS_with_FASPR/linux_example.py
@@ -1,21 +1,32 @@
 import sys
-if len(sys.argv) == 7:
+
+if len(sys.argv) == 8:
 	import time
-	from Pras_Server.PRAS import repairPDB
-	from Pras_Server.FixHeavyAtoms import fixheavyAtoms
-	from Pras_Server.RamaChandra import ramachandranTypes
-	from Pras_Server.SecondaryStructure import assignStructure
+	from Pras_Server.RunType import InitRunType
 	startTime = time.time()
-	
-	fixheavyAtoms(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4], sys.argv[5], sys.argv[6])
 
-	#assignStructure('out_no_h.pdb')
-
-	#ramachandranTypes('out_no_h.pdb')
-	
+	rotamer=sys.argv[2] 
+	mutation=sys.argv[3]
+	pdb_faspr=sys.argv[4]
+	keep_ligand=sys.argv[5]
+	chain_no=sys.argv[6] 
+	ofname=sys.argv[7]
+	fixing=InitRunType(
+					rotamer, 
+					mutation, 
+					pdb_faspr, 
+					keep_ligand, 
+					chain_no,
+					addh=False,
+					ss=False,
+					raman=False,
+					ofname=False,
+					pdbid=False
+					)
+	fixing.fname=sys.argv[1]
+	fixing.ProcessWithDefault()
 	print ('The program took {0} second !'.format(time.time() - startTime))
 
 else:
-	print("PRAS takes 6 compulsory arguments. Execute the code below on your shell prompt to learn more\n")
-
+	print("PRAS takes 8 compulsory arguments. Execute the code below on your shell prompt to learn more\n")
 	print("printf \"from Pras_Server.PRAS import repairPDB\\nprint(repairPDB.__doc__)\\n\\n()\" | python3\n")
