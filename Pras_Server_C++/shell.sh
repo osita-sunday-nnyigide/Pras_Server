069a152d997d31de9d73960f630a9bfa2d32f7dd
osita-sunday-nnyigide <37690739+osita-sunday-nnyigide@users.noreply.github.com>
2023-04-17 22:25:35 +0900

    Add files via upload

--- Pras_Server_C++/shell.sh
+++ Pras_Server_C++/shell.sh
@@ -4,7 +4,7 @@ target="pdb/$1"
 
 for f in "$target"*
 do
-    ./PRAS -i $f -f xxxx -r yes -m yes -l no -h no -p no -s no
+    ./PRAS -i $f -f xxxx -r yes -m yes -l no -h yes -p no -s yes -o $(basename $f) -k yes
     #python3 PlotStructure.py jet 
     echo "fixed" $(basename $f)
     echo " "
