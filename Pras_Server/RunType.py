ff19f20ffe2a32396068b7e0aecc1b6e03f84549
osita-sunday-nnyigide <37690739+osita-sunday-nnyigide@users.noreply.github.com>
2023-05-29 13:56:17 +0900

    Update RunType.py

--- Pras_Server/RunType.py
+++ Pras_Server/RunType.py
@@ -18,7 +18,7 @@ __credits__    = ["Tochukwu Olunna Nnyigide", "Lee Sun-Gu", "Hyun Kyu"]
 
 __license__    = "MIT"
 
-__version__    = "1.0.11"
+__version__    = "1.1.0"
 
 __maintainer__ = "Osita Sunday Nnyigide"
 
@@ -40,7 +40,7 @@ class InitRunType:
 
 	"""
 	fname=None
-	def __init__(self, rotamer="", mutation="", pdb_faspr="", keep_ligand="", chain_no="",addh=False, ss=False, raman=False, ofname=False, pdbid=False):
+	def __init__(self, rotamer="", mutation="", pdb_faspr="", keep_ligand="", chain_no="",addh=False, ss=False, raman=False, ofname=False, pdbid=False, his_p=False):
 		self.ss=ss
 		self.addh=addh 
 		self.raman=raman         
@@ -51,6 +51,7 @@ class InitRunType:
 		self.keep_ligand=keep_ligand
 		self.ofname=ofname
 		self.pdbid=pdbid
+		self.his_p=his_p
 
 	def ProcessWithoutDefault(self):
 		"""
@@ -75,9 +76,10 @@ class InitRunType:
 							chain_no="",
 							addh = False,
 							ss=False,
-							raman=True,
+							raman=False,
 							ofname=False,
-							pdbid=False
+							pdbid=False,
+							his_p=False
 							)
 
 		fixing.ProcessWithoutDefault()
@@ -90,14 +92,14 @@ class InitRunType:
 
 		for i in os.listdir(os.getcwd()):
 			if i.endswith(".pdb") or i.endswith(".ent"):
-				if self.addh:repairPDB( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i[:-4]+'_out.pdb')
+				if self.addh:repairPDB( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i[:-4]+'_out.pdb', self.his_p)
 				else:fixheavyAtoms( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i[:-4]+'_out.pdb')
 				if self.ss:assignStructure(i[:-4]+'_out.pdb')
 				if self.raman:ramachandranTypes(i[:-4]+'_out.pdb')
 				print("fixed {}".format(i))
 
 			elif  i.endswith(".cif"):
-				if self.addh:repairPDB( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i[:-4]+'_out.cif')
+				if self.addh:repairPDB( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i[:-4]+'_out.cif', self.his_p)
 				else:fixheavyAtoms( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i[:-4]+'_out.cif')
 				if self.ss:assignStructure(i[:-4]+'_out.cif')
 				if self.raman:ramachandranTypes(i[:-4]+'_out.cif')
@@ -105,8 +107,8 @@ class InitRunType:
 
 	def ProcessWithoutDefaultUsingPDBID(self):
 		"""
-		Use this member function if you want to process all the PDBs in a pdbid list supplied to PRAS.
-		Here, the name of the output file will be the name of your input PDB id plus _out.
+		Use this member function if you want to process all the PDBs in a PDB ID list supplied to PRAS.
+		Here, the name of the output file will be the name of your input PDB ID plus _out.
 		In the case of a rerun PRAS will remove previous output files because PRAS writes in append mode
 
 		The log file has the name of each PDB repaired so that you will know which PDB structure has missing atoms
@@ -128,7 +130,8 @@ class InitRunType:
 							ss=False,
 							raman=False,
 							ofname=False,
-							pdbid=['1crn','1hen']
+							pdbid=['1crn','1hen'],
+							his_p=False
 							)
 
 		fixing.ProcessWithoutDefaultUsingPDBID()
@@ -138,13 +141,13 @@ class InitRunType:
 		"""
 		if not isinstance(self.pdbid, list):
 			print("You need to supply a list of PDB IDs. PRAS has terminated abnormally")
-			sys.exit()
+			sys.exit(1)
 		for i in os.listdir(os.getcwd()):
 			if i[-8:-4] == '_out' or i == 'log.txt': os.remove(i)
 
 		for i in self.pdbid:
 			if not i.endswith(".cif"):
-				if self.addh:repairPDB( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i+'_out.pdb')
+				if self.addh:repairPDB( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i+'_out.pdb', self.his_p)
 				else:fixheavyAtoms( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i+'_out.pdb')
 				if self.ss:assignStructure(i+'_out.pdb')
 				if self.raman:ramachandranTypes(i+'_out.pdb')
@@ -152,7 +155,7 @@ class InitRunType:
 
 			elif  i.endswith(".cif"):
 				if not self.ofname:self.ofname=i[:-4]+'_out.cif'
-				if self.addh:repairPDB( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i[:-4]+'_out.cif')
+				if self.addh:repairPDB( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i[:-4]+'_out.cif', self.his_p)
 				else:fixheavyAtoms( i, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, i[:-4]+'_out.cif')
 				if self.ss:assignStructure(i[:-4]+'_out.cif')
 				if self.raman:ramachandranTypes(i[:-4]+'_out.cif')
@@ -179,7 +182,8 @@ class InitRunType:
 							ss=False,
 							raman=False,
 							ofname=False,
-							pdbid=False
+							pdbid=False,
+							his_p=False
 							)
 
 		fixing.fname = '1crn.pdb' # change this to whatever your file is named
@@ -191,16 +195,19 @@ class InitRunType:
 		"""
 		if not self.fname:
 			print("You need to supply a file name. PRAS has terminated abnormally")
-			sys.exit()
+			sys.exit(1)
+		if self.ofname:
+			print("Output file name must be set to False. PRAS has terminated abnormally")
+			sys.exit(1)
 		if self.fname.endswith(".pdb") or self.fname.endswith(".ent"):
-			if self.addh:repairPDB(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no)
+			if self.addh:repairPDB(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, self.ofname, self.his_p)
 			else:fixheavyAtoms(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no)
 			if self.ss:assignStructure('out.pdb')
 			if self.raman:ramachandranTypes('out.pdb')
 			print("fixed {}".format(self.fname))
 
 		if self.fname.endswith(".cif"):
-			if self.addh:repairPDB(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no)
+			if self.addh:repairPDB(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, self.ofname, self.his_p)
 			else:fixheavyAtoms(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no)
 			if self.ss:assignStructure('out.cif')
 			if self.raman:ramachandranTypes('out.cif')
@@ -218,43 +225,51 @@ class InitRunType:
 		As an example, copy and run the below code in a python document in your directory with many PDBs
 
 		import os
+		import sys
 		import time
 		from Pras_Server.RunType import InitRunType
 
 		startTime = time.time()
-		out = ['xxx.pdb','zzz.pdb','sss.pdb'] # size of this list MUST be equal to the total number of PDBs
+		out = ['xxxx.pdb','zzzz.pdb','ssss.pdb'] # size of this list MUST be equal to the total number of PDB structures in your working directory
 		k=0
 		for i in os.listdir(os.getcwd()):
-			if i.endswith(".pdb"):
+			if i.endswith(".pdb") or i.endswith(".ent"):
+				try:
+					out[k]
+				except:
+					print('Index error. Size of the name list is small')
+					sys.exit()
 				fixing = InitRunType(rotamer="", 
 									mutation="", 
 									pdb_faspr="", 
 									keep_ligand="", 
 									chain_no="",
 									addh = False,
-									ss=True,
-									raman=True,
+									ss=False,
+									raman=False,
 									ofname=out[k],
-									pdbid=False
+									pdbid=False,
+									his_p=False
 									)
+				fixing.fname = i
 				fixing.ProcessOther()
 				k+=1
 		print ('The program took {0} second !'.format(time.time() - startTime))
 
 		"""
 		if not self.fname:
-			print("You need to supply a file name. PRAS has terminated abnormally")
+			print("No PDB structure file found in your working direcotry. PRAS has terminated abnormally")
 			sys.exit()
 		if self.fname.endswith(".pdb") or self.fname.endswith(".ent"):
-			if self.addh:repairPDB(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no,self.ofname)
+			if self.addh:repairPDB(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, self.ofname, self.his_p)
 			else:fixheavyAtoms(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no,self.ofname)
 			if self.ss:assignStructure(self.ofname)
 			if self.raman:ramachandranTypes(self.ofname)
-			print("fixed {}".format(self.fname))
+			print("fixed {}".format(self.ofname))
 
 		if self.fname.endswith(".cif"):
-			if self.addh:repairPDB(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no,self.ofname)
-			else:fixheavyAtoms(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no,self.ofname)
+			if self.addh:repairPDB(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, self.ofname, self.his_p)
+			else:fixheavyAtoms(self.fname, self.rotamer, self.mutation, self.pdb_faspr, self.keep_ligand, self.chain_no, self.ofname)
 			if self.ss:assignStructure(self.ofname)
 			if self.raman:ramachandranTypes(self.ofname)
-			print("fixed {}".format(self.fname))
+			print("fixed {}".format(self.ofname))
